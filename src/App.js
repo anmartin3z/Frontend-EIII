342b0d2c369f874fe7fcb39fd3ffe55a578ee450
anmarti3z <anmartiez@fpuna.edu.py>
2025-04-04 19:31:39 -0300

    Se agrega el proyecto

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './css/App.css';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+		<div className="App">
+		 
+			<p>HOLA</p>
+		</div>
+	
+    </div>
+  );
+}
+
+export default App;
+
+
